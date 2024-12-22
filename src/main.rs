33c525d4770d78c7f8075ad159a2db2eaa67ef4a
Ibukhvalov <iabukhvalov@edu.hse.ru>
2024-12-23 01:34:31 +0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
+use bytemuck::{Pod, Zeroable};
 use rand::{thread_rng, Rng};
 use wgpu::include_wgsl;
 use wgpu::util::DeviceExt;
 
 
 // max dispatch group size in each dimension is 65535
-const VECTOR_SIZE: usize = 60_000;
+const MATRIX_SIZE: usize = 100;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct Matrix {
+    val: [f32;MATRIX_SIZE*MATRIX_SIZE],
+}
+
+unsafe impl Pod for Matrix {}
+unsafe impl Zeroable for Matrix {}
+
+impl Matrix {
+    fn new_rand() -> Self {
+        let mut rng = thread_rng();
+
+        let mut mat = Self { val: [0f32; MATRIX_SIZE*MATRIX_SIZE] };
+
+        for i in 0..mat.val.len(){
+            mat.val[i] = rng.gen_range(0f32..10f32);
+        }
+
+        mat
+    }
+
+
+    /*fn print(&self) {
+        let size = MATRIX_SIZE;
+        for i in 0..size {
+            for j in 0..size {
+                print!{"{} ", self.val[i*size + j]};
+            }
+            println!();
+        }
+        println!();
+    }*/
+}
+
 
 
 fn main() {
@@ -13,30 +50,20 @@ fn main() {
 
 
 async fn run() {
-    let mut rng = thread_rng();
-    let mut a: [f32; VECTOR_SIZE] = [0f32; VECTOR_SIZE];
-    let mut b: [f32; VECTOR_SIZE] = [0f32; VECTOR_SIZE];
-
-    rng.fill(&mut a[..]);
-    rng.fill(&mut b[..]);
+    let a = Matrix::new_rand();
+    let b = Matrix::new_rand();
 
 
     let result = execute_gpu(&a, &b).await.unwrap();
 
-
-    for i in 0..VECTOR_SIZE {
-        if a[i] + b[i] != result[i] {
-            panic!("{} + {} != {}", a[i], b[i], result[i]);
-        }
-    }
-    println!("All computations completed  correctly!");
-    let rand_index = rng.gen_range(0..VECTOR_SIZE);
-    println!("A random example of result:\n{} + {} = {}", a[rand_index], b[rand_index], a[rand_index]+b[rand_index]);
-
-
+/*    if MATRIX_SIZE < 8 {
+        a.print();
+        b.print();
+        result.print();
+    }*/
 }
 
-async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
+async fn execute_gpu(a: &Matrix, b: &Matrix) -> Option<Matrix> {
     let instance = wgpu::Instance::default();
     let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions::default()).await?;
     let (device, queue) = adapter
@@ -63,12 +90,12 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
 
     let a_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("A Buffer"),
-        contents: bytemuck::cast_slice(&a),
+        contents: bytemuck::bytes_of(a),
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
     });
     let b_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("B Buffer"),
-        contents: bytemuck::cast_slice(&b),
+        contents: bytemuck::bytes_of(b),
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
     });
     let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
@@ -76,6 +103,12 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
         size,
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::COPY_SRC,
         mapped_at_creation: false,
+    });
+
+    let size_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Size buffer"),
+        contents: bytemuck::bytes_of(&MATRIX_SIZE),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
     });
 
 
@@ -87,6 +120,7 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
         compilation_options: Default::default(),
         cache: None,
     });
+
 
     let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
     let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
@@ -104,6 +138,9 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
             wgpu::BindGroupEntry {
                 binding: 2,
                 resource: output_buffer.as_entire_binding(),
+        },  wgpu::BindGroupEntry {
+                binding: 3,
+                resource: size_buffer.as_entire_binding(),
         },]
     });
 
@@ -112,7 +149,7 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
         let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None, timestamp_writes: None });
         cpass.set_pipeline(&compute_pipeline);
         cpass.set_bind_group(0, &bind_group, &[]);
-        cpass.dispatch_workgroups(VECTOR_SIZE as u32, 1, 1);
+        cpass.dispatch_workgroups(MATRIX_SIZE as u32, MATRIX_SIZE as u32, 1);
     }
     encoder.copy_buffer_to_buffer(&output_buffer, 0, &staging_buffer, 0, size);
     queue.submit(Some(encoder.finish()));
@@ -126,11 +163,10 @@ async fn execute_gpu(a: &[f32], b: &[f32]) -> Option<Vec<f32>> {
 
     if let Ok(Ok(())) = receiver.recv_async().await {
         let data_view = buffer_slice.get_mapped_range();
-        let result = bytemuck::cast_slice(&data_view).to_vec();
-
+        let result_view = bytemuck::cast_slice::<_, Matrix>(&data_view);
+        let result = result_view[0].clone();
         drop(data_view);
         staging_buffer.unmap();
-
         Some(result)
     } else {
         panic!("failed to run compute on gpu!");
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,16 +1,37 @@
+struct Matrix {
+    val: array<f32>,
+}
+
+
 @group(0) @binding(0)
-var<storage, read_write> a: array<f32>;
+var<storage, read_write> a: Matrix;
 
 @group(0) @binding(1)
-var<storage, read_write> b: array<f32>;
+var<storage, read_write> b: Matrix;
 
 @group(0) @binding(2)
-var<storage, read_write> output: array<f32>;
+var<storage, read_write> output: Matrix;
 
+@group(0) @binding(3)
+var<uniform> matrix_size: u32;
+
+fn get_index_by_grid(row: u32, column: u32) -> u32 {
+    return row*matrix_size + column;
+}
 
 
 @compute
 @workgroup_size(1)
 fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-    output[global_id.x] = a[global_id.x] + b[global_id.x];
+    var result: f32 = 0.0;
+
+    for(var i: u32 = 0; i < matrix_size; i++) {
+        let a_index = get_index_by_grid(global_id.y, i);
+        let b_index = get_index_by_grid(i, global_id.x);
+        result = result + a.val[a_index] * b.val[b_index];
+    }
+
+    let result_index = get_index_by_grid(global_id.y, global_id.x);
+
+    output.val[result_index] = result;
 }
