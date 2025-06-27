e357fb62d4232d207468f6c851f4356ea50f7026
Ibukhvalov <vivanv345672176@mail.ru>
2025-06-27 23:59:56 +0300

    change script

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,12 +1,17 @@
 import sys
 import torch
 from safetensors.torch import save_file
+from pathlib import Path
 
 if (len(sys.argv) != 2):
     print("Usage: python3 ./generate_data.py <N>")
     sys.exit(1)
 
-torch.manual_seed(1)
+SCRIPT_DIR = Path(__file__).parent.resolve()
+DATA_DIR = SCRIPT_DIR.parent / "data"
+DATA_DIR.mkdir(exist_ok=True)
+OUTPUT_FILE = DATA_DIR / "matmul_data2.safetensors"
+
 
 N = int(sys.argv[1])
 
@@ -18,4 +23,4 @@ save_file({
     "A": A,
     "B": B,
     "C": C
-}, "./data/matmul_data.safetensors")
+}, str(OUTPUT_FILE))
